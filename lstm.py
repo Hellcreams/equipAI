9dc8ddeba936acd03c665b80358e6d31abc7055c
LuTheCat <cconny2002@gmail.com>
2022-11-23 22:24:48 +0900

    neuralNetwork

--- lstm.py
+++ lstm.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from math import e, sqrt
+from math import e, sqrt, tanh
 
 
 def relu(x):
@@ -19,7 +19,7 @@ def xavier_init(column, row, he=False):
     m = 1
     if he:
         m *= 2
-    return np.random.randn(column, row) / sqrt(m/column)
+    return np.random.randn(column, row) / sqrt(m / column)
 
 
 # 참고자료
@@ -50,7 +50,7 @@ class NeuralNetwork:
         self.weight_array_ih = xavier_init(self.hnodes, self.inodes)
         self.weight_array_hh = []
         if self.hidden_layers > 1:
-            for _ in range(hidden_layers-1):
+            for _ in range(hidden_layers - 1):
                 self.weight_array_hh.append(xavier_init(hidden_nodes, hidden_nodes))
         self.weight_array_ho = xavier_init(self.onodes, self.hnodes)
 
@@ -73,18 +73,38 @@ class NeuralNetwork:
 
     def train(self, inputs_list, targets_list):
         # STEP 0. 행렬 변환
+        inputs = np.array(inputs_list, ndmin=2).T
         targets = np.array(targets_list, ndmin=2).T
-        final_outputs = self.query(inputs_list.T)
+
+        # query와 동일
+        hidden_inputs = [np.dot(self.weight_array_ih, inputs)]
+        hidden_outputs = [np.array(list(map(self.activation_function, hidden_inputs))) + self.bias]
+        type(hidden_outputs)
+        for i in range(self.hidden_layers - 1):
+            hidden_inputs.append(np.dot(self.weight_array_hh[i], hidden_outputs))
+            hidden_outputs.append(np.array(list(map(self.activation_function, hidden_inputs[-1]))) + self.bias)
+        final_inputs = np.dot(self.weight_array_ho, hidden_outputs)
+        final_outputs = np.array(list(map(self.activation_function, final_inputs)))
 
         # STEP 1. 출력 계층의 오차 계산
         output_errors = targets - final_outputs
 
         # STEP 2. 은닉 계층의 역전파된 오차 계산
-        hidden_errors = np.dot(self.weight_array_ho.T, output_errors)
+        hidden_errors = [np.dot(self.weight_array_ho.T, output_errors)]
+        for i in range(1, self.hidden_layers):
+            hidden_errors.append((np.dot(self.weight_array_hh[-i].T, hidden_errors[-1])))
+
+        # STEP 3. 가중치 업데이트
+        self.weight_array_ho = self.lr * np.dot((output_errors * sigmoid_diff(final_outputs)), hidden_outputs[-1].T)
+        for i in range(1, self.hidden_layers):
+            self.weight_array_hh[-i] = self.lr * np.dot((hidden_errors[-i] * sigmoid_diff(hidden_outputs[-i])),
+                                                        hidden_outputs[-(i + 1)].T)
+        self.weight_array_ih = self.lr * np.dot((hidden_errors[0] * sigmoid_diff(hidden_outputs[0])), inputs.T)
 
 
 array_sample = np.random.rand(3, 3)
 
-n = NeuralNetwork(2, 3, 1, sigmoid, learning_rate=0.3, hidden_layers=3)
+n = NeuralNetwork(2, 3, 2, sigmoid, learning_rate=0.3, hidden_layers=3)
 
 print(n.query([1.0, 0.5]))
+print(n.train([1.0, 0.5], [1.0, 0.5]))
